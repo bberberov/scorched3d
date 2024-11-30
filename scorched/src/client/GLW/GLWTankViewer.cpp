1d97bc3c81e9020363bdeea9928b80d0ee0fd7a0
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2024-11-29 18:14:04 -0600

    Fixed more spelling errors

--- scorched/src/client/GLW/GLWTankViewer.cpp
+++ scorched/src/client/GLW/GLWTankViewer.cpp
@@ -55,8 +55,8 @@ GLWTankViewer::GLWTankViewer(float x, float y, int numH, int numV) :
 	catagoryChoice_.setHandler(this);
 
 	catagoryChoice_.setToolTip(new ToolTip(ToolTip::ToolTipHelp, 
-		LANG_RESOURCE("MODEL_CATAGORY", "Model Category"),
-		LANG_RESOURCE("MODEL_CATAGORY_TOOLTIP", 
+		LANG_RESOURCE("MODEL_CATEGORY", "Model Category"),
+		LANG_RESOURCE("MODEL_CATEGORY_TOOLTIP",
 		"Displays the currently selected model category.\n"
 		"To make models easier to locate\n"
 		"tank models are grouped by category.")));
