d35e652dcbe1f949f54d2242622c2adcb11e8494
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2025-04-04 12:39:04 -0600

    Added initial GitHub CI configuration and scripts

---
+++ .ci/prepare-macos.bash
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# Install necessary packages
+brew install \
+	meson \
+	\
+	expat \
+	fftw \
+	freealut \
+	freetype2 \
+	glew \
+	mesa-glu \
+	jpeg-turbo \
+	libogg \
+	openal-soft \
+	libpng \
+	sdl12-compat \
+	libvorbis \
+	wxwidgets \
+	zlib
---
+++ .ci/prepare-windows.bat
@@ -0,0 +1,13 @@
+python -m pip install meson
+
+cd scorched
+
+meson wrap install expat
+meson wrap install freetype2
+meson wrap install glew
+meson wrap install libjpeg-turbo
+meson wrap install libpng
+meson wrap install ogg
+meson wrap install openal-soft
+meson wrap install vorbis
+meson wrap install zlib
