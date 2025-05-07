e0348b277e892828def4aec2adc282a6c6baa1a6
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2025-05-07 15:46:09 -0600

    Added more dependencies to Meson Build

--- .ci/prepare-macos.bash
+++ .ci/prepare-macos.bash
@@ -8,13 +8,18 @@ brew install \
 	fftw \
 	freealut \
 	freetype2 \
+	\
 	glew \
 	mesa-glu \
 	jpeg-turbo \
+	\
 	libogg \
 	openal-soft \
 	libpng \
 	sdl12-compat \
+	sdl2_net \
 	libvorbis \
 	wxwidgets \
 	zlib
+
+meson wrap install lua
