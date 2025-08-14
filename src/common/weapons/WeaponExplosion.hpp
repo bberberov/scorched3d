6444560436653686f816bc314546b09347abcc2e
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2025-08-14 17:20:09 -0600

    Fixed typo in `src/common/weapons/WeaponExplosion.hpp`

--- src/common/weapons/WeaponExplosion.hpp
+++ src/common/weapons/WeaponExplosion.hpp
@@ -19,7 +19,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #if !defined(AFX_WEAPONEXPLOSION_H__70119A64_2064_4066_8EE5_FD6A3E24D5FC__INCLUDED_)
-#define AFX_WEAPONPEXPLOSION_H__70119A64_2064_4066_8EE5_FD6A3E24D5FC__INCLUDED_
+#define AFX_WEAPONEXPLOSION_H__70119A64_2064_4066_8EE5_FD6A3E24D5FC__INCLUDED_
 
 #include <weapons/Weapon.hpp>
 #include <actions/ExplosionParams.hpp>
