64318c4d562a190164e9ce4d0a66a3a6c0da9dde
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2026-05-12 12:45:31 -0600

    Test `vcpkg-tool-meson`

--- .ci/prepare-windows.bat
+++ .ci/prepare-windows.bat
@@ -1,18 +1,9 @@
 REM Install necessary packages
 REM python -m pip install meson
 vcpkg install vcpkg-tool-meson
-vcpkg owns meson
-vcpkg owns meson.py
-
-vcpkg env --tools "vcpkg-tool-meson"
-
 vcpkg install pkgconf
-vcpkg owns pkgconf
-vcpkg owns pkg-config
-vcpkg owns pkgconf.exe
-vcpkg owns pkg-config.exe
 
-vcpkg env --tools "pkgconf"
+vcpkg env --tools "meson setup --backend vs2022 --buildtype debugoptimized builddir"
 
 rem  vcpkg install expat
 rem  vcpkg install fftw3
