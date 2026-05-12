44d63762cdc2e525b90977e087c084136bed1ec6
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2026-05-12 16:40:19 -0600

    Test

--- .ci/prepare-windows.bat
+++ .ci/prepare-windows.bat
@@ -3,13 +3,16 @@ python -m pip install meson
 vcpkg install pkgconf
 vcpkg owns pkgconf.exe
 
-rem  dir C:\vcpkg
+vcpkg install ^
+	lua[cpp]
+
 rem  dir C:\vcpkg\installed
-rem  dir C:\vcpkg\installed\x64-windows
-rem  dir C:\vcpkg\installed\x64-windows\include\pkgconf\libpkgconf
-rem  dir C:\vcpkg\installed\x64-windows\lib\pkgconfig
-rem  dir C:\vcpkg\installed\x64-windows\share\pkgconf
-rem  dir C:\vcpkg\installed\x64-windows\share\vcpkg-tool-meson
+dir C:\vcpkg\installed\x64-windows
+dir C:\vcpkg\installed\x64-windows\include
+dir C:\vcpkg\installed\x64-windows\lib
+dir C:\vcpkg\installed\x64-windows\lib\pkgconfig
+dir C:\vcpkg\installed\x64-windows\share
+dir C:\vcpkg\installed\x64-windows\share\pkgconf
 rem  dir C:\vcpkg\installed\x64-windows\tools\pkgconf
 rem  dir C:\vcpkg\packages
 rem  dir C:\vcpkg\packages\vcpkg-tool-meson_x64-windows\share\vcpkg-tool-meson
@@ -18,23 +21,23 @@ rem  dir C:\vcpkg\packages\pkgconf_x64-windows\share\pkgconf
 rem  dir C:\vcpkg\packages\pkgconf_x64-windows\tools\pkgconf
 rem  type C:\vcpkg\scripts\buildsystems\meson\meson.template.in
 
-vcpkg install ^
-	expat ^
-	fftw3 ^
-	freealut ^
-	freetype[png,zlib] ^
-	opengl ^
-	glew ^
-	^
-	libjpeg-turbo ^
-	lua[cpp] ^
-	libogg ^
-	openal-soft ^
-	libpng ^
-	sdl1 ^
-	sdl1-net ^
-	libvorbis ^
-	wxwidgets ^
-	zlib
+rem  vcpkg install ^
+rem  	expat ^
+rem  	fftw3 ^
+rem  	freealut ^
+rem  	freetype[png,zlib] ^
+rem  	opengl ^
+rem  	glew ^
+rem  	^
+rem  	libjpeg-turbo ^
+rem  	lua[cpp] ^
+rem  	libogg ^
+rem  	openal-soft ^
+rem  	libpng ^
+rem  	sdl1 ^
+rem  	sdl1-net ^
+rem  	libvorbis ^
+rem  	wxwidgets ^
+rem  	zlib
 
 vcpkg owns expat.pc
