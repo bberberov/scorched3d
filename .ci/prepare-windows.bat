158fc80c2a5eaa0650a50a0deb3cf34973ffca25
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2026-05-12 14:17:57 -0600

    Test

--- .ci/prepare-windows.bat
+++ .ci/prepare-windows.bat
@@ -1,18 +1,24 @@
 rem  Install necessary packages
 python -m pip install meson
 vcpkg install pkgconf
-vcpkg owns pkgconf
 vcpkg owns pkgconf.exe
 
-dir C:\vcpkg
-dir C:\vcpkg\installed
+rem  dir C:\vcpkg
+rem  dir C:\vcpkg\installed
 dir C:\vcpkg\installed\x64-windows
-dir C:\vcpkg\packages
-dir C:\vcpkg\packages\vcpkg-tool-meson_x64-windows
-dir C:\vcpkg\packages\pkgconf_x64-windows
-dir C:\vcpkg\scripts\buildsystems
+dir C:\vcpkg\installed\x64-windows\bin
+dir C:\vcpkg\installed\x64-windows\include
+dir C:\vcpkg\installed\x64-windows\lib
+dir C:\vcpkg\installed\x64-windows\share
+dir C:\vcpkg\installed\x64-windows\tools
+rem  dir C:\vcpkg\packages
+dir C:\vcpkg\packages\vcpkg-tool-meson_x64-windows\share
+dir C:\vcpkg\packages\pkgconf_x64-windows\lib
+dir C:\vcpkg\packages\pkgconf_x64-windows\share
+dir C:\vcpkg\packages\pkgconf_x64-windows\tools
+dir C:\vcpkg\scripts\buildsystems\meson
 
-rem  vcpkg install expat
+vcpkg install expat
 rem  vcpkg install fftw3
 rem  vcpkg install freealut
 rem  vcpkg install freetype
