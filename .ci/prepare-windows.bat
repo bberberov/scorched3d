81db934119ab17c8ebaca5647c9047abf8447449
Boian Berberov <7432115+bberberov@users.noreply.github.com>
2026-05-12 12:51:30 -0600

    Test `vcpkg env`

--- .ci/prepare-windows.bat
+++ .ci/prepare-windows.bat
@@ -1,24 +1,21 @@
-REM Install necessary packages
-REM python -m pip install meson
-vcpkg install vcpkg-tool-meson
+rem  Install necessary packages
+python -m pip install meson
 vcpkg install pkgconf
 
-vcpkg env --tools "meson setup --backend vs2022 --buildtype debugoptimized builddir"
+vcpkg install expat
+vcpkg install fftw3
+vcpkg install freealut
+vcpkg install freetype
+vcpkg install opengl
+vcpkg install glew
 
-rem  vcpkg install expat
-rem  vcpkg install fftw3
-rem  vcpkg install freealut
-rem  vcpkg install freetype
-rem  vcpkg install opengl
-rem  vcpkg install glew
-rem
-rem  vcpkg install libjpeg-turbo
-rem  vcpkg install lua
-rem  vcpkg install libogg
-rem  vcpkg install openal-soft
-rem  vcpkg install libpng
-rem  vcpkg install sdl1
-rem  vcpkg install sdl1-net
-rem  vcpkg install libvorbis
-rem  vcpkg install wxwidgets
-rem  vcpkg install zlib
+vcpkg install libjpeg-turbo
+vcpkg install lua
+vcpkg install libogg
+vcpkg install openal-soft
+vcpkg install libpng
+vcpkg install sdl1
+vcpkg install sdl1-net
+vcpkg install libvorbis
+vcpkg install wxwidgets
+vcpkg install zlib
